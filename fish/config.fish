ad7bbc7a0e6d2e096aee0bae9fa19b962fe45c78
nukkua <marco.salazar.2014.com@gmail.com>
2025-10-25 19:52:26 -0400

    done

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,7 @@ set -x LS_COLORS 'di=1;38;5;218:fi=0'
 
 alias grep='grep --color=auto'
 alias recon-ng='/home/leverna/packages/recon-ng/recon-ng'
-alias vi='nvim'
+alias im='nvim'
 alias wmqttx='nohup flatpak run com.emqx.MQTTX &> /dev/null &'
 alias sortty='python3 /usr/local/bin/sortty-bin/sortty.py'
 alias php82='/usr/local/php82/bin/php'
@@ -15,6 +15,7 @@ alias fold='~/scripts/fzf-oldfiles.sh'
 alias zen='~/packages/zen/zen'
 alias lett='~/lettbar/zig-out/bin/lettbar'
 alias sslscan='/home/leverna/packages/sslscan/sslscan'
+alias roblox='flatpak run org.vinegarhq.Sober'
 
 alias beef='/home/leverna/packages/beef-master/beef'
 
@@ -35,12 +36,17 @@ set -x PATH $CARGO_HOME/bin $PATH
 # CUDA
 set -x LD_LIBRARY_PATH /opt/cuda/lib64 $LD_LIBRARY_PATH
 
+set -x ANDROID_HOME "$HOME/Android/Sdk"
+set -x ANDROID_SDK_ROOT "$HOME/Android/Sdk"
+
 # bun
 set -x BUN_INSTALL "$HOME/.bun"
 set -x PATH $BUN_INSTALL/bin $PATH
 
 # zoxide
 zoxide init fish | source
+# fzf
+fzf --fish | source
 
 
 # editor
@@ -51,6 +57,8 @@ set -x VISUAL nvim
 # LLDB
 set -x LLDB_USE_NATIVE_PDB_READER "yes"
 abbr --add zbr zig build run
+abbr --add zz z ..
 
 bind \ch backward-word
 bind \cl forward-word
+
